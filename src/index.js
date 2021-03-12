81db564cd915af508838bdbf9a73fffda6568ed8
Semitsu Takayuki <Semitsu@semitsu.net>
2021-03-13 00:03:43 +0900

    [feature] render road2

--- src/index.js
+++ src/index.js
@@ -25,14 +25,92 @@ const terrain_position_array = [
   [3, 2],
   [4, 2],
   [0, 3],
-  [1,3],
+  [1, 3],
   [2, 3],
   [3, 3],
   [1, 4],
-  [2,4],
-  [3,4]
+  [2, 4],
+  [3, 4]
 ]
 
+const road_x_offset = 33;
+const road_y_offset = -10;
+const road_grid_width = 68;
+const road_grid_height = 60;
+const road_position_dict = {
+  "0": [2, 0],
+  "1": [3, 0],
+  "2": [4, 0],
+  "3": [5, 0],
+  "4": [6, 0],
+  "5": [7, 0],
+  "6": [2, 1],
+  "7": [4, 1],
+  "8": [6, 1],
+  "9": [8, 1],
+  "10": [1, 2],
+  "11": [2, 2],
+  "12": [3, 2],
+  "13": [4, 2],
+  "14": [5, 2],
+  "15": [6, 2],
+  "16": [7, 2],
+  "17": [8, 2],
+  "18": [1, 3],
+  "19": [3, 3],
+  "20": [5, 3],
+  "21": [7, 3],
+  "22": [9, 3],
+  "23": [0, 4],
+  "24": [1, 4],
+  "25": [2, 4],
+  "26": [3, 4],
+  "27": [4, 4],
+  "28": [5, 4],
+  "29": [6, 4],
+  "30": [7, 4],
+  "31": [8, 4],
+  "32": [9, 4],
+  "33": [0, 5],
+  "34": [2, 5],
+  "35": [4, 5],
+  "36": [6, 5],
+  "37": [8, 5],
+  "38": [10, 5],
+  "39": [0, 6],
+  "40": [1, 6],
+  "41": [2, 6],
+  "42": [3, 6],
+  "43": [4, 6],
+  "44": [5, 6],
+  "45": [6, 6],
+  "46": [7, 6],
+  "47": [8, 6],
+  "48": [9, 6],
+  "49": [1, 7],
+  "50": [3, 7],
+  "51": [5, 7],
+  "52": [7, 7],
+  "53": [9, 7],
+  "54": [1, 8],
+  "55": [2, 8],
+  "56": [3, 8],
+  "57": [4, 8],
+  "58": [5, 8],
+  "59": [6, 8],
+  "60": [7, 8],
+  "61": [8, 8],
+  "62": [2, 9],
+  "63": [4, 9],
+  "64": [6, 9],
+  "65": [8, 9],
+  "66": [2, 10],
+  "67": [3, 10],
+  "68": [4, 10],
+  "69": [5, 10],
+  "70": [6, 10],
+  "71": [7, 10],
+}
 
 // https://ja.reactjs.org/docs/rendering-elements.html#updating-the-rendered-element
 function tick() {
@@ -100,6 +178,30 @@ function TerrainHexa (props) {
     // </div>
   )
 }
+function Road (props) {
+  let class_name = "road";
+  let x = parseInt(props.x);
+  let y = parseInt(props.y);
+  if ((y+4) % 4 == 0) {
+    class_name += x % 2 == 0 ? " rup" : " lup"
+  }
+  else if ((y+4) % 4 == 2){
+    class_name += x % 2 == 0 ? " lup" : " rup"
+  }else {
+    class_name += " road-oddrow"
+  }
+  let left = road_x_offset + road_grid_width * parseInt(x);
+  let top = road_y_offset + road_grid_height * parseInt(y);
+  // http://www.tohoho-web.com/css/prop/transform-origin.htm
+  // transform_originを設定すること
+  return (
+    <div class={class_name} style={{top: top + 'px', left: left + 'px'}}>
+    <button class="roadcontent"
+      onClick={() => props.onClick()}
+    >{x}, {y}</button>
+    </div>
+  )
+}
 // TODO: class="hexaone" text comes to top, not within div box. 
 class CatanBoard extends React.Component{
     renderTerrainHexa(i) {
@@ -117,11 +219,96 @@ class CatanBoard extends React.Component{
         />
       )
     }
+    renderRoad(i){
+      let x = "";
+      let y = "";
+      [x, y] = road_position_dict[`${i}`]
+      return (
+        <Road 
+          // TODO: onClick to be onClickRoad()?
+          onClick={() => this.props.onClick(i)} 
+          x={x}
+          y={y}
+        ></Road>
+      )
+    }
     render() {
         return (
             <div>
             <h1><Tick /></h1>
             <h1><Welcome name={tick()}/></h1>
+            <div>{this.renderRoad(0)}</div>
+            <div>{this.renderRoad(1)}</div>
+            <div>{this.renderRoad(2)}</div>
+            <div>{this.renderRoad(3)}</div>
+            <div>{this.renderRoad(4)}</div>
+            <div>{this.renderRoad(5)}</div>
+            <div>{this.renderRoad(6)}</div>
+            <div>{this.renderRoad(7)}</div>
+            <div>{this.renderRoad(8)}</div>
+            <div>{this.renderRoad(9)}</div>
+            <div>{this.renderRoad(10)}</div>
+            <div>{this.renderRoad(11)}</div>
+            <div>{this.renderRoad(12)}</div>
+            <div>{this.renderRoad(13)}</div>
+            <div>{this.renderRoad(14)}</div>
+            <div>{this.renderRoad(15)}</div>
+            <div>{this.renderRoad(16)}</div>
+            <div>{this.renderRoad(17)}</div>
+            <div>{this.renderRoad(18)}</div>
+            <div>{this.renderRoad(19)}</div>
+            <div>{this.renderRoad(20)}</div>
+            <div>{this.renderRoad(21)}</div>
+            <div>{this.renderRoad(22)}</div>
+            <div>{this.renderRoad(23)}</div>
+            <div>{this.renderRoad(24)}</div>
+            <div>{this.renderRoad(25)}</div>
+            <div>{this.renderRoad(26)}</div>
+            <div>{this.renderRoad(27)}</div>
+            <div>{this.renderRoad(28)}</div>
+            <div>{this.renderRoad(29)}</div>
+            <div>{this.renderRoad(30)}</div>
+            <div>{this.renderRoad(31)}</div>
+            <div>{this.renderRoad(32)}</div>
+            <div>{this.renderRoad(33)}</div>
+            <div>{this.renderRoad(34)}</div>
+            <div>{this.renderRoad(35)}</div>
+            <div>{this.renderRoad(36)}</div>
+            <div>{this.renderRoad(37)}</div>
+            <div>{this.renderRoad(38)}</div>
+            <div>{this.renderRoad(39)}</div>
+            <div>{this.renderRoad(40)}</div>
+            <div>{this.renderRoad(41)}</div>
+            <div>{this.renderRoad(42)}</div>
+            <div>{this.renderRoad(43)}</div>
+            <div>{this.renderRoad(44)}</div>
+            <div>{this.renderRoad(45)}</div>
+            <div>{this.renderRoad(46)}</div>
+            <div>{this.renderRoad(47)}</div>
+            <div>{this.renderRoad(48)}</div>
+            <div>{this.renderRoad(49)}</div>
+            <div>{this.renderRoad(50)}</div>
+            <div>{this.renderRoad(51)}</div>
+            <div>{this.renderRoad(52)}</div>
+            <div>{this.renderRoad(53)}</div>
+            <div>{this.renderRoad(54)}</div>
+            <div>{this.renderRoad(55)}</div>
+            <div>{this.renderRoad(56)}</div>
+            <div>{this.renderRoad(57)}</div>
+            <div>{this.renderRoad(58)}</div>
+            <div>{this.renderRoad(59)}</div>
+            <div>{this.renderRoad(60)}</div>
+            <div>{this.renderRoad(61)}</div>
+            <div>{this.renderRoad(62)}</div>
+            <div>{this.renderRoad(63)}</div>
+            <div>{this.renderRoad(64)}</div>
+            <div>{this.renderRoad(65)}</div>
+            <div>{this.renderRoad(66)}</div>
+            <div>{this.renderRoad(67)}</div>
+            <div>{this.renderRoad(68)}</div>
+            <div>{this.renderRoad(69)}</div>
+            <div>{this.renderRoad(70)}</div>
+            <div>{this.renderRoad(71)}</div>
             <div class="honeycomb">
             <div class="ibws-fix">
               <div class="hexaone">
